411568019103166fb0be8099068f3d5d8a4e85b2
xi_guang <3285778581@qq.com>
2022-12-02 17:11:54 +0800

    查询单词

---
+++ 查询单词/usr/bin/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+headers = {
+    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5'
+}
+def look_up():
+    word = input('请输入要查询的单词')
+    res = requests.get(f'http://api.tangdouz.com/fy.php?nr={word}',headers=headers)
+    res.encoding = 'utf-8'
+    print(res.text)
+while 1:
+    look_up()

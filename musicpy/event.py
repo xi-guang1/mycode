fefc638746c83d2a210fb6113be2b887e5ac8d52
xi-guang1 <3285778581@qq.com>
2022-12-04 17:51:21 +0800

    update

--- musicpy/event.py
+++ musicpy/event.py
@@ -1,7 +1,5 @@
 from musicpy import *
 
 
-def btn_play_onclick(note_lst, durlst, durlst_1):
-    print(durlst)
-    print(durlst_1)
-    play(chord(note_lst, durlst, durlst_1))
+def btn_play_onclick(note_lst, durlst, durlst_1,save_as_file=False):
+    play(chord(note_lst, durlst, durlst_1),bpm=120,save_as_file=save_as_file)

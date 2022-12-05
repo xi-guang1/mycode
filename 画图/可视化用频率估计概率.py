225788e1417bf36bfbcadf716372601fa4538f4e
xi-guang1 <3285778581@qq.com>
2022-12-05 10:10:03 +0800

    new file

---
+++ 画图/可视化用频率估计概率.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from random import randint
+from tqdm import tqdm
+
+x = range(1,int(input('请输入试验次数')) + 1)
+y = []
+up = 0
+
+for i in tqdm(x):
+    up += randint(0,1)
+    y.append(up/i)
+
+plt.plot(x,y)
+plt.show()

f3f6d2b2ab688e7136ff2951b738d25a821ee0c1
汐光 <3285778581@qq.com>
2022-12-12 12:58:41 +0800

    updata

--- 扫雷脚本/windows版本/main.py
+++ 扫雷脚本/windows版本/main.py
@@ -8,9 +8,15 @@ from PIL import Image
 import pyautogui
 import ctypes
 import cv2
+import os
 
 
 def get_img(window_name):
+    
+    path = './images/'
+    if not os.path.exists(path):
+        # 如果不存在，创建它
+        os.makedirs(path)
     
     # 获取软件窗口的句柄
     hwnd = win32gui.FindWindow(None, window_name)
@@ -27,7 +33,7 @@ def get_img(window_name):
     cDC.BitBlt((0, 0), (ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)), dcObj, (0, 0), win32con.SRCCOPY)
 
     # 保存软件窗口图像
-    dataBitMap.SaveBitmapFile(cDC, "window_image.bmp")
+    dataBitMap.SaveBitmapFile(cDC, path + "window_image.bmp")
 
     # 释放资源
     dcObj.DeleteDC()
@@ -35,10 +41,28 @@ def get_img(window_name):
     win32gui.ReleaseDC(hwnd, wDC)
     win32gui.DeleteObject(dataBitMap.GetHandle())
     
-    img = cv2.imread("window_image.bmp")
-    print(img.shape)
+    img = Image.open(path + "window_image.bmp")
+    img = img.crop((35,75,205,245))
+    img.save(path + "window_image.bmp")
+
+    # 计算每个正方形的尺寸
+    square_size = 17
+
+    # 遍历每行每列的正方形
+    for row in range(10):
+        for col in range(10):
+            # 计算左上角和右下角坐标
+            left = col * square_size
+            top = row * square_size
+            right = left + square_size
+            bottom = top + square_size
+
+            # 裁剪正方形
+            square = img.crop((left, top, right, bottom))
+
+            # 保存正方形
+            square.save(path + f'image_{row}_{col}.png')
     
-    return "window_image.bmp"
+    return path + "window_image.bmp"
     
-img = get_img('扫雷')
-print(img)
+get_img('扫雷')
